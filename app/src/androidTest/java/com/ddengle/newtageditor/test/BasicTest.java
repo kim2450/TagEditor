47a0eb9ee3f0c820e1069ab8d9fe19bf3f793ffc
김성우 <kim.sungwoo@adways.kr>
2015-07-28 12:34:19 +0900

    first commit

---
+++ app/src/androidTest/java/com/ddengle/newtageditor/test/BasicTest.java
@@ -0,0 +1,17 @@
+package com.ddengle.newtageditor.test;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by swkim on 2014-11-19.
+ */
+public class BasicTest extends InstrumentationTestCase {
+
+    public void testSimple(){
+        assertEquals(2,2);
+    }
+
+    public void testSimple_1(){
+        assertEquals(5,5);
+    }
+}
